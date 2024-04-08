1c5b5aa040556925e39d1cc6ae24b09f83043f26
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-04-08 11:47:08 +0400

    gradient

--- shaders/mandelbrot_fs.glsl
+++ shaders/mandelbrot_fs.glsl
@@ -1,6 +1,6 @@
 #version 330
 
-#define MAX_ITERATIONS 50
+#define MAX_ITERATIONS 1000
 #define BOUNDED_THRESHOLD 2.0
 
 in vec2 coord;
@@ -15,15 +15,15 @@ vec2 complex_product(vec2 a, vec2 b) {
 }
 
 void main() {
-    // Compute iterations
-    vec2 z = vec2(0.0);
-    for (int i = 0; i < MAX_ITERATIONS; i++) {
-        z = complex_product(z, z) + coord;
-    }
+    // Initial condition
+    vec2 z = vec2(0.0); // z0 = 0 + 0i
 
-    // Check if z is bounded
-    if (distance(z, coord) < BOUNDED_THRESHOLD)
-        FragColor = vec4(1.0);
-    else
-        FragColor = vec4(vec3(0.0), 1.0);
+    // Compute iterations
+    int i = 0;
+    while (i++ < MAX_ITERATIONS && length(z) < BOUNDED_THRESHOLD)
+        z = complex_product(z, z) + coord; // f(z) = z^2 + c
+
+    float val = i < MAX_ITERATIONS ? 1 - float(i) / MAX_ITERATIONS : 0.0;
+    vec3 bgColor = vec3(0.3, 0.3, 0.7);
+    FragColor = vec4(bgColor * val, 1.0);
 }
