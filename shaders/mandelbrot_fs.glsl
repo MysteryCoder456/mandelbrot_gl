898b577e33096ffd4dbfd557b91976b644bef36a
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-04-08 10:37:09 +0400

    basic mandelbrot shape

--- shaders/mandelbrot_fs.glsl
+++ shaders/mandelbrot_fs.glsl
@@ -1,10 +1,27 @@
 #version 330
 
+#define MAX_ITERATIONS 10
+
 in vec2 coord;
 out vec4 FragColor;
 
+// Product of two complex numbers
+// Function treats x component as real and y component as imaginary
+vec2 complex_product(vec2 a, vec2 b) {
+    float real = a.x * b.x - a.y * b.y;
+    float imag = a.x * b.y + a.y * b.x;
+    return vec2(real, imag);
+}
+
 void main() {
-    // TODO: implement Mandelbrot fractal logic
-    float val = float(int(coord.x) % 10 == 0 || int(coord.y) % 10 == 0);
-    FragColor = vec4(val * 0.1, val * 0.1, val * 0.3, 1.0);
+    vec2 z = vec2(0.0);
+
+    for (int i = 0; i < MAX_ITERATIONS; i++) {
+        z = complex_product(z, z) + coord;
+    }
+
+    if (distance(z, coord) < 2.0)
+        FragColor = vec4(1.0);
+    else
+        FragColor = vec4(vec3(0.0), 1.0);
 }
