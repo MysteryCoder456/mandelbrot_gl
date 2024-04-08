7d6a03b66a416c5c0a0fb60d3c61a77029befc5a
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-04-08 11:06:47 +0400

    some comments

--- shaders/mandelbrot_fs.glsl
+++ shaders/mandelbrot_fs.glsl
@@ -1,6 +1,7 @@
 #version 330
 
 #define MAX_ITERATIONS 50
+#define BOUNDED_THRESHOLD 2.0
 
 in vec2 coord;
 out vec4 FragColor;
@@ -14,13 +15,14 @@ vec2 complex_product(vec2 a, vec2 b) {
 }
 
 void main() {
+    // Compute iterations
     vec2 z = vec2(0.0);
-
     for (int i = 0; i < MAX_ITERATIONS; i++) {
         z = complex_product(z, z) + coord;
     }
 
-    if (distance(z, coord) < 2.0)
+    // Check if z is bounded
+    if (distance(z, coord) < BOUNDED_THRESHOLD)
         FragColor = vec4(1.0);
     else
         FragColor = vec4(vec3(0.0), 1.0);
